dae0ad9041d590f206346dd4d9fe99637659c472
Zadorozhnyi Kostiantyn <zadorozhnyikostiantyn@gmail.com>
2025-05-30 22:45:30 +0300

    implemented noise generator

---
+++ noise-generator/src/audio/audio.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Arduino.h>
+
+void setupAudio();
+void startPlayback(String filename);
+void stopPlayback();
+void startRecording(String filename);
+void stopRecording();
+
+extern bool isPlaying; 
+extern bool isRecording; 
+extern bool isUltrasonic; 
+
+
---
+++ noise-generator/src/display/display.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <WString.h>
+
+void setupDisplay();
+void drawMessage(String message);
